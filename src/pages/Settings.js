129bdf7022a0c2ff52315afd887cd1c2ddd92e62
BShrestha <41086302+ijyapu@users.noreply.github.com>
2023-08-01 18:03:44 -0400

    Update Settings.js

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Settings.css'; // Import your CSS file for custom styling
 
-function Settings() {
+const Settings = () => {
   return (
-    <div className='settings'>
-        <h1>Settings</h1>
+    <div className="settings-container">
+      <h2>Settings</h2>
+      <div className="settings-buttons">
+        <Link to="/account">
+          <button className="settings-button">Account</button>
+        </Link>
+        <Link to="/about">
+          <button className="settings-button">About Us</button>
+        </Link>
+        <Link to="/logout">
+          <button className="settings-button">Log Out</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
